c81645896ac662dfd97de94187ae3561e279d9de
Scott Pritchard <Scott.pritchard@korelogic.co.uk>
2020-02-24 12:06:28 +0000

    Updated emptyCaches batch file

--- EmptyCaches.bat
+++ EmptyCaches.bat
@@ -1,4 +1,4 @@
-E:\EmptyStandbyList.exe workingsets
-E:\EmptyStandbyList.exe modifiedpagelist
-E:\EmptyStandbyList.exe priority0standbylist
-E:\EmptyStandbyList.exe standbylist
+%~dp0/EmptyStandbyList.exe workingsets
+%~dp0/EmptyStandbyList.exe modifiedpagelist
+%~dp0/EmptyStandbyList.exe priority0standbylist
+%~dp0/EmptyStandbyList.exe standbylist
